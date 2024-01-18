05171fbc39cc5ac11c011c3a8356dfc5d183b152
Aritra Baidik <aritrabaidik@gmail.com>
2024-01-18 13:37:13 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const multer = require('multer');
+const xlsx = require('xlsx');
+const { MongoClient } = require('mongodb');
+const cors = require('cors')
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT;
+
+const allowedOrigins = [
+    ''
+];
+
+app.use(cors({
+    origin:function(origin,callback){
+        if(allowedOrigins.indexOf(origin)!== -1 || !origin){
+            callback(null,true);
+        }
+        else{
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials:true,
+}))
+
+// Multer setup for handling file uploads
+const storage = multer.memoryStorage(); // Store the file in memory
+const upload = multer({ storage: storage });
+
+app.post('/upload', upload.single('file'), async (req, res) => {
+  try {
+    const fileBuffer = req.file.buffer;
+
+    // Read the Excel file from the buffer
+    const workbook = xlsx.read(fileBuffer, { type: 'buffer' });
+    const sheetName = workbook.SheetNames[0];
+    const sheet = workbook.Sheets[sheetName];
+    const dataArray = xlsx.utils.sheet_to_json(sheet, { header: 1 });
+
+    // Assuming the first row contains the column headers
+    const headerRow = dataArray[0];
+
+    // Prepare data for MongoDB insertion
+    const dataToInsert = dataArray.slice(1).map(row => {
+      const rowData = {};
+      headerRow.forEach((header, index) => {
+        rowData[header] = row[index];
+      });
+      return rowData;
+    });
+
+    // Call the function to insert data into MongoDB
+    await insertDataIntoMongoDB(dataToInsert);
+
+    res.status(200).send('File uploaded and data inserted into MongoDB.');
+  } catch (error) {
+    console.error('Error processing file and inserting data:', error);
+    res.status(500).send('Error processing file and inserting data.');
+  }
+});
+
+async function insertDataIntoMongoDB(data) {
+  const uri = process.env.MONGO_URI;
+  const client = new MongoClient(uri);
+
+  try {
+    await client.connect();
+    const database = client.db();
+    const collection = database.collection('student_data'); // Replace with your actual collection name
+
+    // Insert data into MongoDB
+    const result = await collection.insertMany(data);
+
+    console.log(`${result.insertedCount} documents inserted into MongoDB`);
+  } finally {
+    await client.close();
+  }
+}
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
